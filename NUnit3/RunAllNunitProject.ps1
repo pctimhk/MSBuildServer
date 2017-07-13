afc84c9e5c0036a0798130c6e4d4502b06ab639f
tt1366 <yim.tim@bcthk.com>
2017-07-13 10:41:02 +0800

    Enhance nunit powershell to log the nunit error output and display nunit output to console.

--- NUnit3/RunAllNunitProject.ps1
+++ NUnit3/RunAllNunitProject.ps1
@@ -1,7 +1,3 @@
-<#
-This script will execute Nunit3 console program for all the *.nunit Nunit configuration project file.
-The test result will generate out and the result file name same as the Nunit configuration project filename.
-#>
 
 $RootPath=$args[0]
 $TestOutputPath=$args[1]
@@ -18,10 +14,12 @@ $filenames = Get-ChildItem "$RootPath" -Recurse -Include *.nunit -Exclude *Sprin
 foreach($file in $filenames) {
     $OutputXML = $TestOutputPath + $file.BaseName + ".xml";
     $OutputText = $TestOutputPath + $file.BaseName + ".txt";
+    $OutputErrorText = $TestOutputPath + $file.BaseName + "_err.txt";
     $FileFolder = (Convert-Path $file.PSParentPath);
-    $NunitArgs = '"'+$file.FullName+'" --trace=Verbose -work="'+$FileFolder+'" --result="'+$OutputXML+';format=nunit3" --output "'+$OutputText+'"';
-    $FullCommand = $NunitCommand + " " + $NunitArgs;
-    Write-Host "Execute Nunit for '$file'";
-    Start-Process -FilePath "$NunitCommand" -ArgumentList $NunitArgs -Wait;
-    Write-Host "Result output to '$OutputXML'";
+    $NunitArgs = '"'+$file.FullName+'" --trace=Verbose -work="'+$FileFolder+'" --result="'+$OutputXML+';format=nunit3" --output "'+$OutputText+'" --err="'+$OutputErrorText+'"';
+    $FullCommand = "'$NunitCommand' $NunitArgs";
+    Write-Host 'Execute Nunit project "'+$file+'" on '+ $(Get-Date);
+    #Start-Process -FilePath "$NunitCommand" -ArgumentList $NunitArgs -Wait;
+    & $($NunitCommand) $NunitArgs
+    Write-Host "Result output to '$OutputXML' on " + $(Get-Date);
 }
