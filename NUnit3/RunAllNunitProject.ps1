fd5a6b6a987ee1a33a154e047eb75f48002bb43b
tt1366 <yim.tim@bcthk.com>
2017-07-13 15:18:49 +0800

    Enhance powershell script to support multiple thread NUnit test.

--- NUnit3/RunAllNunitProject.ps1
+++ NUnit3/RunAllNunitProject.ps1
@@ -4,22 +4,26 @@ $TestOutputPath=$args[1]
 
 $NunitCommand = 'D:\Program Files (x86)\NUnit.org\nunit-console\nunit3-console.exe'
 
-
 if(!(Test-Path -Path $TestOutputPath )){
     New-Item -ItemType directory -Path $TestOutputPath;
 }
 
-$filenames = Get-ChildItem "$RootPath" -Recurse -Include *.nunit -Exclude *Spring.Testing.NUnit*
-
-foreach($file in $filenames) {
-    $OutputXML = $TestOutputPath + $file.BaseName + ".xml";
-    $OutputText = $TestOutputPath + $file.BaseName + ".txt";
-    $OutputErrorText = $TestOutputPath + $file.BaseName + "_err.txt";
-    $FileFolder = (Convert-Path $file.PSParentPath);
-    $NunitArgs = '"'+$file.FullName+'" --trace=Verbose -work="'+$FileFolder+'" --result="'+$OutputXML+';format=nunit3" --output "'+$OutputText+'" --err="'+$OutputErrorText+'"';
-    $FullCommand = "'$NunitCommand' $NunitArgs";
-    Write-Host 'Execute Nunit project "'+$file+'" on '+ $(Get-Date);
-    #Start-Process -FilePath "$NunitCommand" -ArgumentList $NunitArgs -Wait;
-    & $($NunitCommand) $NunitArgs
-    Write-Host "Result output to '$OutputXML' on " + $(Get-Date);
+workflow StartNUnitTest {
+    param ($NunitCommand, $Filenames, $TestOutputPath)
+    foreach -parallel($file in $Filenames)
+    {
+        $OutputXML = $TestOutputPath + $file.BaseName + ".xml";
+        $OutputText = $TestOutputPath + $file.BaseName + ".txt";
+        $OutputErrorText = $TestOutputPath + $file.BaseName + "_err.txt";
+        $FileFolder = (Convert-Path $file.PSParentPath);
+        $FilePath = $file.FullName;
+        $NunitArgs = '"'+$FilePath+'" --trace=Verbose -work="'+$FileFolder+'" --result="'+$OutputXML+';format=nunit3" --output "'+$OutputText+'" --err="'+$OutputErrorText+'"';
+        $FullCommand = """$NunitCommand"" $NunitArgs";
+        Invoke-Expression -Command "echo 'Execute Nunit project $file on $(Get-Date)'";
+        Invoke-Expression "& ""$NunitCommand"" $NunitArgs";
+        Invoke-Expression -Command "echo 'Result output to $OutputXML on $(Get-Date)'";
+    }
 }
+
+$filenames = Get-ChildItem "$RootPath" -Recurse -Include *.nunit -Exclude *Spring.Testing.NUnit*;
+StartNUnitTest -NunitCommand $NunitCommand -Filenames $filenames -TestOutputPath $TestOutputPath;
