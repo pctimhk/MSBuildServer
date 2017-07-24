edc584472ae7f5c09b8bbceb2ee8143327f95da9
tt1366 <yim.tim@bcthk.com>
2017-07-24 10:04:02 +0800

    Integrate OpenCover with nunit test and generate html report.

--- OpenCover/RunAllNunitProject.ps1
+++ OpenCover/RunAllNunitProject.ps1
@@ -1,31 +1,46 @@
-param ($RootPath = 'D:\Program Files (x86)\Jenkins\jobs\SCS-DEV\workspace\default\', $TestOutputPath = 'D:\Program Files (x86)\Jenkins\jobs\SCS-DEV\workspace\default\TestOutput\', $NunitCommand = 'D:\Program Files (x86)\NUnit.org\nunit-console\nunit3-console.exe', $OpenCoverCommand = 'D:\Program Files (x86)\opencover_4_6_519\OpenCover.Console.exe')
+param (
+    $RootPath = 'D:\Program Files (x86)\Jenkins\jobs\SCS-DEV\workspace\default', 
+    $TestOutputPath = 'D:\Program Files (x86)\Jenkins\jobs\SCS-DEV\workspace\default\TestOutput', 
+    $NunitCommand = 'D:\Program Files (x86)\NUnit.org\nunit-console\nunit3-console.exe', 
+    $OpenCoverCommand = 'D:\Program Files (x86)\opencover_4_6_519\OpenCover.Console.exe',
+    $OpenCoverReportGenerator = 'D:\Program Files (x86)\OpenCover_ReportGenerator_2_5_10\ReportGenerator.exe'
+)
 
 if(!(Test-Path -Path $TestOutputPath )){
     New-Item -ItemType directory -Path $TestOutputPath;
 }
 
 workflow StartNUnitTest {
-    param ($OpenCoverCommand, $NunitCommand, $Filenames, $TestOutputPath)
+    param ($OpenCoverCommand, $NunitCommand, $Filenames, $NUnitOutputFolder, $OpenCoverOutputFolder)
     foreach -parallel($file in $Filenames)
     {
-        $TempBatchCmd = $TestOutputPath + $file.BaseName + ".bat";
+        $TempBatchCmd = $NUnitOutputFolder + "\" + $file.BaseName + ".bat";
 
-        $OutputXML = $TestOutputPath + $file.BaseName + ".xml";
-        $OutputText = $TestOutputPath + $file.BaseName + ".txt";
-        $OutputErrorText = $TestOutputPath + $file.BaseName + "_err.txt";
-        $OpenCoverOutputXml = $TestOutputPath + $file.BaseName + "_OC.xml";
+        $OutputXML = $NUnitOutputFolder + "\NUnit\" + $file.BaseName + "_NU.xml";
+        $OutputText = $NUnitOutputFolder  + "\NUnit\" +  $file.BaseName + ".txt";
+        $OutputErrorText = $NUnitOutputFolder  + "\NUnit\" +  $file.BaseName + "_err.txt";
+        $OpenCoverOutputXml = $NUnitOutputFolder  + "\OpenCover\" +  $file.BaseName + "_OC.xml";
         $FileFolder = (Convert-Path $file.PSParentPath);
         $FilePath = $file.FullName;
-        #$NunitArgs = '"'+$FilePath+'" --trace=Verbose -work="'+$FileFolder+'" --result="'+$OutputXML+';format=nunit3" --output "'+$OutputText+'" --err="'+$OutputErrorText+'"';
-        $Command = """$OpenCoverCommand"" -register:user -target:""$NunitCommand"" -targetargs:""""""$FilePath"""" "+'--'+"trace=Verbose -work=""""$FileFolder"""" "+'--'+"output """"$OutputText"""" "+'--'+"err=""""$OutputErrorText"""" "+'--'+"result=""""$OutputXML;format=nunit2"""" ""  -filter:+[*]* -output:""$OpenCoverOutputXml""";
+        $Command = """$OpenCoverCommand"" -register:user -target:""$NunitCommand"" -targetargs:""""""$FilePath"""" "+'--'+"trace=Verbose -work=""""$FileFolder"""" "+'--'+"output """"$OutputText"""" "+'--'+"err=""""$OutputErrorText"""" "+'--'+"result=""""$OutputXML;format=nunit2"""" ""  -filter:+[*]* -output:""$OpenCoverOutputXml""`n";
         $FullCommand = """$NunitCommand"" $NunitArgs";
         Invoke-Expression -Command "echo 'Execute Nunit project $file on $(Get-Date)'";
         Out-File -FilePath "$TempBatchCmd" -InputObject $Command -Encoding ASCII -Force;
         Invoke-Expression "& ""$TempBatchCmd""";
-        #Invoke-Expression "& ""$NunitCommand"" $NunitArgs";
         Invoke-Expression -Command "echo 'Result output to $OutputXML on $(Get-Date)'";
     }
 }
 
+$NUnitOutputFolder = $TestOutputPath + "\NUnit";
+$OpenCoverOutputFolder = $TestOutputPath  + "\OpenCover";
+$OpenCoverHtmlOutputFolder = $TestOutputPath  + "\OpenCoverHtml";
+
+# call workflow to generate every nunit project
 $filenames = Get-ChildItem "$RootPath" -Recurse -Include *.nunit -Exclude *Spring.Testing.NUnit*;
-StartNUnitTest -OpenCoverCommand $OpenCoverCommand -NunitCommand $NunitCommand -Filenames $filenames -TestOutputPath $TestOutputPath;
+StartNUnitTest -OpenCoverCommand $OpenCoverCommand -NunitCommand $NunitCommand -OpenCoverReportGenerator $OpenCoverReportGenerator -Filenames $filenames -NUnitOutputFolder $NUnitOutputFolder -OpenCoverOutputFolder $OpenCoverOutputFolder;
+
+# generate html report
+$TempBatchCmd = $NUnitOutputFolder + "\GenerateOpenCoverHtmlReport.bat";
+$Command = """$OpenCoverReportGenerator"" -reports:""$OpenCoverOutputFolder\*.xml"" -targetDir:""$OpenCoverHtmlOutputFolder""`n";
+Out-File -FilePath "$TempBatchCmd" -InputObject $Command -Encoding ASCII -Force;
+Invoke-Expression "& ""$TempBatchCmd""";
--- README.md
+++ README.md
@@ -13,7 +13,13 @@ Prerequisite
 - Windows SDK 7 SDK
 - Windows SDK 8.1
 - Microsoft WebDeploy 3.6
-- Open Cover
+- Open Cover 4.6.519
+    https://github.com/OpenCover/opencover/releases
+- Open Cover Report Generator 2.5.10
+    https://github.com/danielpalme/ReportGenerator/releases
+- OpenCoverToCoberturaConverter 2.0.4
+    https://github.com/danielpalme/OpenCoverToCoberturaConverter
+    
 
 Folder
 =============
@@ -21,3 +27,4 @@ NUnit3  - All program or script for NUnit3
 - Powershell script to execute Nunit3 console program for all the *.nunit Nunit configuration project file. The test result will generate out and the result file name same as the Nunit configuration project filename.
 
 OpenCover
+- Execute nunit test with open cover and html report generation
