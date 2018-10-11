801604ff0b4c1f1cd8b56a28ad190d1a4cd99f27
tt1366 <yim.tim@bcthk.com>
2018-10-11 17:27:42 +0800

    Enhance unzip to UNC program where fail deployment procedure if error found.

--- JenkinsExtension/unziptopath.ps1
+++ JenkinsExtension/unziptopath.ps1
@@ -10,12 +10,29 @@ $BinFolderPath = "$folderPath\$fileName"
 if(Test-Path -Path $BinFolderPath ) {
     Remove-Item "$BinFolderPath" -Force -Recurse
 }
-
-$command = "`"D:\Programs\JenkinsExtension\7z.bat`" x `"$zipPath`" -o`"$BinFolderPath`""
-iex "& $command"
-
-if (![string]::IsNullOrEmpty($remoteUNC))
+Try 
 {
-    Write-Output "Copy unzip folder from $BinFolderPath to $remoteUNC"
-    Copy-Item -Force -Recurse "$BinFolderPath\*" -Destination "$remoteUNC"
+    $command = "`"D:\Programs\JenkinsExtension\7z.bat`" x `"$zipPath`" -o`"$BinFolderPath`""
+    Invoke-Expression "& $command"
+
+    if (!($lastExitCode) -eq 0) {
+        Write-Host "unzip file error." -ForegroundColor Red
+        exit 1
+        break
+    }
+
+
+    if (![string]::IsNullOrEmpty($remoteUNC))
+    {
+        Write-Output "Copy unzip folder from $BinFolderPath to $remoteUNC"
+        Copy-Item -Force -Recurse "$BinFolderPath\*" -Destination "$remoteUNC"
+    }
+}
+Catch 
+{
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    Write-Host "Error found. Error: $ErrorMessage, Item: $FailedItem" -ForegroundColor Red
+    exit 1
+    break
 }
